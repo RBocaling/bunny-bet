59023f19178cb805425191ef6bb7e53b82b529f9
reynaldo <reynaldofbocaling@gmail.com>
2025-04-01 16:22:53 +0800

    upload

--- client/src/sagas/home.js
+++ client/src/sagas/home.js
@@ -1,38 +1,39 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { bringPayload, showPayload } from "../reducers/home";
+import { API_URL } from "../config";
 
-function request(){
-    return api().then(function(res){
-        return res
-    })
+function request() {
+  return api().then(function (res) {
+    return res;
+  });
 }
 
-function api(){
-    return new Promise(function(resolve, reject){
-        fetch('/api/home', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-              "Accept": "application/json",
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({})
-        })
-        .then((response) => response.json())
-        .then((data) => resolve(data))
-        .catch(err => console.error('fetchBringThemAll2--> ' + err));
+function api() {
+  return new Promise(function (resolve, reject) {
+    fetch(`${API_URL}/api/home`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
     })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((err) => console.error("fetchBringThemAll2--> " + err));
+  });
 }
 
-function* fetchBringPayload(){
-    try{        
-        const output = yield call(request)
-        yield put(showPayload(output))
-    } catch(error){
-        console.log('fetchBringThemAll1--> ', error)
-    }   
+function* fetchBringPayload() {
+  try {
+    const output = yield call(request);
+    yield put(showPayload(output));
+  } catch (error) {
+    console.log("fetchBringThemAll1--> ", error);
+  }
 }
 
 export function* homeRegister() {
-    yield takeLatest(bringPayload.type, fetchBringPayload)
+  yield takeLatest(bringPayload.type, fetchBringPayload);
 }
